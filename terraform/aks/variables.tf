fc08c2cefedfa311cdafbce38aa5b1f71a1ea340
Elou <elouen.lft@gmail.com>
2026-06-29 14:09:26 +0200

    TP1: infra AKS Terraform

---
+++ terraform/aks/variables.tf
@@ -0,0 +1,31 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group EXISTANT, fourni par l'école."
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "aks-formation"
+}
+
+variable "node_count" {
+  type    = number
+  default = 2
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B2s"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    formation = "aks"
+    module    = "1-fondamentaux"
+  }
+}
